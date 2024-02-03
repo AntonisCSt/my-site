9531bee7c1e0721f9cb1823c5da952b817a6a354
AntonisCSt <a.x-stellas@hotmail.com>
2024-02-03 12:55:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+
+            // Check if the target is in the same page or a different page
+            if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+                // Same page
+                scrollToSection(targetId);
+            } else {
+                // Different page
+                window.location.href = this.getAttribute('href');
+            }
+        });
+    });
+
+    // Function to scroll to the target section smoothly
+    function scrollToSection(targetId) {
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+});
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://6xm3u6crbopx5piqmdztnmkjle0dziil.lambda-url.us-east-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
+}
+updateCounter();
