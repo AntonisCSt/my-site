a10f1547508657309a2b1fde02861caf4cfc21a9
AntonisCSt <a.x-stellas@hotmail.com>
2024-04-06 23:22:43 +0300

    added night day functionality

--- website/script.js
+++ website/script.js
@@ -38,3 +38,30 @@ async function updateCounter() {
     counter.innerHTML = `Visits: ${data}`;
 }
 updateCounter();
+
+
+
+/*-------------------Night/Day Button -------------------*/
+
+// Check for saved theme preference and apply it
+if(localStorage.getItem('theme') === 'night') {
+    document.body.classList.add('night-mode');
+}
+
+    // Assume you have FontAwesome loaded in your project
+    document.getElementById('theme-toggle').addEventListener('click', function() {
+    // Toggle the 'night-mode' class
+    document.body.classList.toggle('night-mode');
+
+    // Check if the night mode is active to determine which icon to display
+    if(document.body.classList.contains('night-mode')) {
+        this.innerHTML = '<i class="fa fa-moon-o"></i>'; // Sun icon for day mode switch
+        localStorage.setItem('theme', 'day');
+    } else {
+        this.innerHTML = '<i class="fa fa-sun-o"></i>'; // Moon icon for night mode switch
+        localStorage.setItem('theme', 'night');
+    }
+});;
+
+
+
